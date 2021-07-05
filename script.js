4e13fe1c1ce5d4f2d03032dbf6dc114b714aa523
Hazel Lim <Hazel@Admins-MacBook-Pro-3.local>
2021-07-05 17:46:09 +0800

    Drawing with emojis - modes - added input validation in main function lines 129-140 and default welcome message lines 74-89

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // global variables to store game modes
+var GAME_MODE_INPUT_MODES = "GAME_MODE_INPUT_MODES";
 var GAME_MODE_NUMBER_OF_CHARACTERS = "GAME_MODE_NUMBER_OF_CHARACTERS";
 var GAME_MODE_SQUARE = "GAME_MODE_SQUARE";
 var GAME_MODE_TRIANGLE = "GAME_MODE_TRIANGLE";
@@ -69,47 +70,78 @@ var drawTriangle = function (input) {
 };
 
 var main = function (input) {
-  var myOutputValue = "";
-
-  // convert input to lowercase
-  var inputLower = input.toLowerCase();
-
-  if (inputLower.contains("characters")) {
-    // change game mode to GAME_MODE_NUMBER_OF_CHARACTERS
-    gameMode = GAME_MODE_NUMBER_OF_CHARACTERS;
+  // Default output value
+  if (input == "") {
+    var myOutputValue =
+      "Hello! There are 3 game modes in this game: <br>" +
+      "1) characters <br>" +
+      "2) square <br>" +
+      "3) triangle <br>" +
+      "Please enter your choice of game modes by either entering the number index, or the name of the game modes ";
+    console.log("default output");
+    console.log(myOutputValue);
+    // change gameMode to GAME_MODE_INPUT_MODES
+    gameMode = GAME_MODE_INPUT_MODES;
     console.log("game mode");
     console.log(gameMode);
 
-    // prompt user to enter integer value
-    myOutputValue =
-      "You chose the game mode: Number of characters. Next, enter an integer value for the number of characters.";
+    return myOutputValue;
   }
-  if (inputLower.contains("square")) {
-    // change game mode to GAME_MODE_SQUARE
-    gameMode = GAME_MODE_SQUARE;
-    console.log("game mode");
-    console.log(gameMode);
 
-    // prompt user to enter integer value
-    myOutputValue =
-      "You chose the game mode: square. Next, enter an integer value for the dimensions of the square.";
-  }
-  if (inputLower.contains("triangle")) {
-    // change game mode to GAME_MODE_TRIANGLE
-    gameMode = GAME_MODE_TRIANGLE;
-    console.log("game mode");
-    console.log(gameMode);
+  // if gameMode is GAME_MODE_INPUT_MODES, get the user input for gameMode
+  if (gameMode == GAME_MODE_INPUT_MODES) {
+    // convert input to lowercase letters
+    var inputLower = input.toLowerCase();
 
-    // prompt user to enter integer value
-    myOutputValue =
-      "You chose the game mode: triangle. Next, enter an integer value for the height of the triangle.";
+    if (inputLower == "characters" || input == 1) {
+      // change game mode to GAME_MODE_NUMBER_OF_CHARACTERS
+      gameMode = GAME_MODE_NUMBER_OF_CHARACTERS;
+      console.log("game mode");
+      console.log(gameMode);
+
+      // prompt user to enter integer value
+      myOutputValue =
+        "You chose the game mode: <b>Number of characters</b>." +
+        "<br>" +
+        "Next, enter an integer value for the number of characters.";
+    } else if (inputLower == "square" || input == 2) {
+      // change game mode to GAME_MODE_SQUARE
+      gameMode = GAME_MODE_SQUARE;
+      console.log("game mode");
+      console.log(gameMode);
+
+      // prompt user to enter integer value
+      myOutputValue =
+        "You chose the game mode: <b>square</b>." +
+        "<br>" +
+        "Next, enter an integer value for the dimensions of the square.";
+    } else if (inputLower == "triangle" || input == 3) {
+      // change game mode to GAME_MODE_TRIANGLE
+      gameMode = GAME_MODE_TRIANGLE;
+      console.log("game mode");
+      console.log(gameMode);
+
+      // prompt user to enter integer value
+      myOutputValue =
+        "You chose the game mode: <b>triangle</b>." +
+        "<br>" +
+        "Next, enter an integer value for the height of the triangle.";
+    } else {
+      // If it reaches this point, input is invalid. Prompt user to enter a valid input
+      console.log("Invalid input");
+      myOutputValue =
+        "Oops, please enter the following to access the respective game modes:" +
+        "<br>" +
+        "<b> GAME MODE: characters </b> => enter '1' or 'characters'" +
+        "<br>" +
+        "<b> GAME MODE: square </b> => enter '2' or 'square'" +
+        "<br>" +
+        "<b> GAME MODE: triangle </b> => enter '3' or 'triangle'";
+    }
+    return myOutputValue;
   }
 
   if (input == "" || Number(input) == NaN) {
-    // Output default value "hello world" if user does not enter a number
-    myOutputValue = "hello world";
-    console.log("default output");
-    console.log(myOutputValue);
   }
 
   return myOutputValue;
