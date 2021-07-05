c24c00ea1442329b90c6ab594509209f1da44455
Hazel Lim <Hazel@Admins-MacBook-Pro-3.local>
2021-07-05 16:31:12 +0800

    Drawing with Emojis - Square

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = "";
+
+  // Create loop to output "input" number of thumbs-up emoji
+  var index = 0;
+  while (index < input) {
+    myOutputValue += "👍";
+    index += 1;
+  }
+
+  // Output default value "hello world" if user does not enter a number
+
+  if (input == "" || Number(input) == NaN) {
+    myOutputValue = "hello world";
+  }
+
   return myOutputValue;
 };
